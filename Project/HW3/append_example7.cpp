3801c3f7942d2bc022917a7b002fdb5a45bb4adf
De Yu <cyu521@gmail.com>
2017-03-18 13:01:37 -0400

    added mouse click to start rotate

--- Project/HW3/append_example7.cpp
+++ Project/HW3/append_example7.cpp
@@ -32,15 +32,23 @@ void sunMove(int test){
 	double radian = angle * 3.14 / 180.0;
 	sunPosition = vec4(cos(radian)*20, sin(radian)*-4, 0, 0);//rotate around the plan
 
-	mat4 t1(vec4(1, 0, 0, 0), vec4(0, 1, 0, 0), vec4(0, 0, 1, zPos), vec4(0, 0, 0, 1));
-	mat4 t2(vec4(1, 0, 0, 0), vec4(0, 1, 0, 0), vec4(0, 0, 1, -zPos), vec4(0, 0, 0, 1));
-	for (int i = 0; i < cubes.size(); i++){
-		double theta = ((double)rand());
-		cubes[i]->setModelMatrix(t1*RotateX(theta)*t2);
-	}
 
 	glutPostRedisplay();
 	glutTimerFunc(250, sunMove, 0);
+}
+bool startRotate = false;
+void startGame(int value){
+	if (startRotate){
+		mat4 t1(vec4(1, 0, 0, 0), vec4(0, 1, 0, 0), vec4(0, 0, 1, zPos), vec4(0, 0, 0, 1));
+		mat4 t2(vec4(1, 0, 0, 0), vec4(0, 1, 0, 0), vec4(0, 0, 1, -zPos), vec4(0, 0, 0, 1));
+		for (int i = 0; i < cubes.size(); i++){
+			double theta = ((double)rand());
+			cubes[i]->setModelMatrix(t1*RotateX(theta)*t2);
+		}
+	}
+
+	glutPostRedisplay();
+	glutTimerFunc(250, startGame, 0);
 }
 
 //current cube v,
@@ -265,74 +273,12 @@ reshape(int width, int height)
 		currentCam->proj = Perspective(fov, aspect, 1.0, 100);
 	glutPostRedisplay();
 }
-bool rayTriangleIntersect(
-	const vec4 &orig4, const vec4 &dir4,
-	const vec4 &v04, const vec4 &v14, const vec4 &v24,
-	float &t, float &u, float &v)
-{
-	vec3 orig(orig4.x, orig4.y, orig4.z);
-	vec3 dir(dir4.x, dir4.y, dir4.z);
-	vec3 v0(v04.x, v04.y, v04.z);
-	vec3 v1(v14.x, v14.y, v14.z);
-	vec3 v2(v24.x, v24.y, v24.z);
-	// compute plane's normal
-	vec3 v0v1 = v1 - v0;
-	vec3 v0v2 = v2 - v0;
-	// no need to normalize
-	vec3 N = cross(v0v1, v0v2); // N 
-	float denom = dot(N, N);
-
-	// Step 1: finding P
-
-	// check if ray and plane are parallel ?
-	float NdotRayDirection = dot(N, dir); 
-	if (fabs(NdotRayDirection) < 1e-8) // almost 0 
-		return false; // they are parallel so they don't intersect ! 
-
-	// compute d parameter using equation 2
-	float d = -dot(N, v0); 
-
-	// compute t (equation 3)
-	t = (dot(N, orig)  +d) / NdotRayDirection;
-	// check if the triangle is in behind the ray
-	if (t < 0) return false; // the triangle is behind 
-
-	// compute the intersection point using equation 1
-	vec3 P = orig + t * dir;
-
-	// Step 2: inside-outside test
-	vec3 C; // vector perpendicular to triangle's plane 
-
-	// edge 0
-	vec3 edge0 = v1 - v0;
-	vec3 vp0 = P - v0;
-	C = cross(edge0, vp0);
-	if (dot(N, C) < 0) return false; // P is on the right side 
-
-	// edge 1
-	vec3 edge1 = v2 - v1;
-	vec3 vp1 = P - v1;
-	C = cross(edge1, vp1);
-	if ((u = dot(N, C)) < 0)  return false; // P is on the right side 
-
-	// edge 2
-	vec3 edge2 = v0 - v2;
-	vec3 vp2 = P - v2;
-	C = cross(edge2, vp2);
-	if ((v = dot(N, C)) < 0) return false; // P is on the right side; 
-
-	u /= denom;
-	v /= denom;
-
-	return true; // this ray hits the triangle 
-}
 
 void mouse(GLint button, GLint state, GLint x, GLint y){
 	//create object on mouse release
 	if (GLUT_UP == state){
-		double xpos = 2.0 * double(x / 512.0) - 1.0;
-		double ypos = 1 - 2.0 * double(y / 512.0);
-		glutPostRedisplay();
+		startRotate = !startRotate;
+		glutTimerFunc(250, startGame, 0);
 	}
 }
 //----------------------------------------------------------------------------
